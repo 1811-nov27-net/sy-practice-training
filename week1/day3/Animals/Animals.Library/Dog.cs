ed702936a4db6115847bcf9411b293bba616b13d
justinzsy <justinzsy@gmail.com>
2018-11-28 15:26:53 -0600

    add more classes to animal demo.

--- week1/day3/Animals/Animals.Library/Dog.cs
+++ week1/day3/Animals/Animals.Library/Dog.cs
@@ -2,14 +2,40 @@
 
 namespace Animals.Library
 {
-    public class Dog
+    public class Dog : IAnimal
     {
         // not fields - properties
-        public string Name {get; set;}
+
+        // only auto-properties have implicit backing fields.
+        // as soon as you give a body to the get or the set,
+        // you need to add a private field yourself.
+
+        public int Id {get; set;}
+        public string Name 
+        {
+            // getters and setters can contain arbitrary logic
+            get {
+                return "Bob";
+            }
+            set {
+                Console.WriteLine("inside property setter");
+            }
+        }
 
         // auto-property
-        public string Breed {get; set;}
-
+        private string _breed;
+        public string Breed 
+        {
+            get {
+                return _breed;
+            }
+            set {
+                // validation - no null or empty allowed.
+                if (value != null && value != "") {
+                    _breed = value;
+                }
+            }
+        }
         // property (with explicit backing field)
         private int _age;
         public int Age
@@ -18,7 +44,8 @@ namespace Animals.Library
             {
                 return _age;
             }
-            set
+            // get and set can have access modifiers of their own
+            private set
             {
                 _age = value;
                 // keyword "value" in a setter takes in the assigned value
@@ -39,6 +66,21 @@ namespace Animals.Library
         public void Bark()
         {
             Console.WriteLine("Woof");
+        }
+
+        public void MakeSound()
+        {
+            Bark();
+        }
+
+        public void GoTo(string location)
+        {
+            // string interpolation syntax
+            //    prefix with $ (changes the meaning ofthe {})
+            //    inside {} you should give an expression
+            //       either string or any other object that ToString will be called on
+            string output = $"Walking to {location.ToLower()}.";
+            Console.WriteLine(output);
         }
 
         // snippets: prop, propfull
