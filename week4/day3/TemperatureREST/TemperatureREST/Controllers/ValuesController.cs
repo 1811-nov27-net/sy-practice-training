d741dd3ddbe548477fb5c8a25bac6862d2f637f2
justinzsy <justinzsy@gmail.com>
2018-12-19 11:55:05 -0600

    REST and PostMan practice, text was too small to try and follow

---
+++ week4/day3/TemperatureREST/TemperatureREST/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TemperatureREST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            // whatever we return as an ActionResult will be automatically
+            // serialized as JSON.
+            // when making REST services, we do not use Views at all, or any HTML,
+            // or any view rendering step - we just respond with the data in JSON format.
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
